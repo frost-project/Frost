3f7ba73d8510fd1f9e2ff7a080f609fdf784a073
Spider-Admin <Spider-Admin@proton.me>
2025-08-28 21:03:36 +0200

    Refactor FreenetPriority.getPriority

--- src/main/java/frost/fileTransfer/FreenetPriority.java
+++ src/main/java/frost/fileTransfer/FreenetPriority.java
@@ -32,18 +32,13 @@ public enum FreenetPriority {
 	public static String getName(final int priority) {
 		return getPriority(priority).getName();
 	}
-	
+
 	static public FreenetPriority getPriority(final int priority) {
-		switch(priority) {
-			case 0: return MAXIMUM; 
-			case 1: return VERY_HIGH; 
-			case 2: return HIGH; 
-			case 3: return MEDIUM; 
-			case 4: return LOW; 
-			case 5: return VERY_LOW; 
-			case 6: return PAUSE;
-			default :
-				throw new IllegalArgumentException("No such priority");
+		for (FreenetPriority value : values()) {
+			if (value.getNumber() == priority) {
+				return value;
+			}
 		}
+		throw new IllegalArgumentException("No such priority");
 	}
 }
